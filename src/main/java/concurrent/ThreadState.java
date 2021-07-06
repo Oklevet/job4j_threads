1812bdf42b45c44490dbfdab1fd848239e8800ce
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-07-06 16:57:55 +0500

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,20 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        first.start();
+        second.start();
+        while (first.isAlive() || second.isAlive()) {}
+        System.out.println("done.");
+    }
+}
