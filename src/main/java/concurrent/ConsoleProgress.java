29a84af1c45f1d4b69a61cbdc471350948d4ae09
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-07-09 23:48:00 +0500

    3. Прерывание нити [#1019 #174879]

--- src/main/java/concurrent/ConsoleProgress.java
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -1,15 +1,15 @@
 package concurrent;
 
 public class ConsoleProgress implements Runnable {
-    String[] process = new String[] {"\\", "\\|", "\\|/" };
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000);
+        Thread.sleep(20000);
         progress.interrupt();
     }
     @Override
     public void run() {
+        String[] process = new String[] {"\\", "\\|", "\\|/" };
         while (!Thread.currentThread().isInterrupted()) {
             for (int i = 0; i < process.length; i++) {
                 System.out.print("\r load: " + process[i]);
