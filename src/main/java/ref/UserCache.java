3c29799e105cb9cca5a22f5b8ac9c1ee21850a9f
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-07-14 13:22:35 +0500

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ref/UserCache.java
@@ -0,0 +1,27 @@
+package ref;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        for (int i : users.keySet()) {
+            list.add(User.of(users.get(i).getName()));
+        }
+        return list;
+    }
+}
