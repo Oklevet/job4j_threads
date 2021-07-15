dc187c8a4c9a5f25af2e879d6411dc3ed72ede0f
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-07-15 15:32:14 +0500

    3. Прерывание нити [#1019 #174879]

--- src/main/java/concurrent/ConsoleProgress.java
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -10,14 +10,16 @@ public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
         String[] process = new String[] {"\\", "\\|", "\\|/" };
+        int i = 0;
         while (!Thread.currentThread().isInterrupted()) {
-            for (int i = 0; i < process.length; i++) {
-                System.out.print("\r load: " + process[i]);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            System.out.print("\r load: " + process[i++]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (i == process.length) {
+                i = 0;
             }
         }
     }
