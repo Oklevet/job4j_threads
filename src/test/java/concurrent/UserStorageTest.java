da6893133e0a323e1d481158b2e2bb00d06c2c59
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-08-29 18:32:27 +0500

    update by project errors

--- src/test/java/concurrent/UserStorageTest.java
+++ src/test/java/concurrent/UserStorageTest.java
@@ -1,15 +1,42 @@
 package concurrent;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import javax.jws.soap.SOAPBinding;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class UserStorageTest {
+
+    @Test
+    public void whenDel() throws InterruptedException {
+        UserStorage storage = new UserStorage();
+        Thread one = new ThreadAdd1(storage);
+        Thread two = new ThreadAdd2(storage);
+        one.start();
+        two.start();
+        one.join();
+        two.join();
+        User expeceted1 = storage.get(1);
+        User expeceted2 = storage.get(2);
+        storage.delete(storage.get(1));
+        assertEquals(expeceted2, storage.get(2));
+        assertNotEquals(expeceted1, storage.get(1));
+    }
+
+    @Test
+    public void whenTh1TransferTh2() throws InterruptedException {
+        UserStorage storage = new UserStorage();
+        Thread one = new ThreadAdd1(storage);
+        Thread two = new ThreadAdd2(storage);
+        one.start();
+        two.start();
+        one.join();
+        two.join();
+        storage.transfer(1, 2, 500);
+        assertEquals(200, storage.get(1).getAmount());
+        assertEquals(1000, storage.get(2).getAmount());
+    }
+
     private static class ThreadAdd1 extends Thread {
         private final UserStorage storage;
 
@@ -39,35 +66,5 @@ public class UserStorageTest {
             }
             this.storage.add(new User(2, 500));
         }
-    }
-
-    @Test
-    public void WhenDel() throws InterruptedException {
-        UserStorage storage = new UserStorage();
-        Thread one = new ThreadAdd1(storage);
-        Thread two = new ThreadAdd2(storage);
-        one.start();
-        two.start();
-        one.join();
-        two.join();
-        User expeceted1 = storage.get(1);
-        User expeceted2 = storage.get(2);
-        storage.delete(storage.get(1));
-        assertEquals(expeceted2, storage.get(2));
-        assertNotEquals(expeceted1, storage.get(1));
-    }
-
-    @Test
-    public void WhenTh1TransferTh2() throws InterruptedException {
-        UserStorage storage = new UserStorage();
-        Thread one = new ThreadAdd1(storage);
-        Thread two = new ThreadAdd2(storage);
-        one.start();
-        two.start();
-        one.join();
-        two.join();
-        storage.transfer(1, 2, 500);
-        assertEquals(200, storage.get(1).getAmount());
-        assertEquals(1000, storage.get(2).getAmount());
     }
 }
