5164ca370c8926524693614e56778da71170d5b6
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-08-29 19:31:54 +0500

    3. Класс хранилища пользователей UserStorage [#1104 #191669]

--- src/main/java/concurrent/UserStorage.java
+++ src/main/java/concurrent/UserStorage.java
@@ -1,26 +1,25 @@
 package concurrent;
 
-import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
     private final HashMap<Integer, User> storage = new HashMap();
-    @GuardedBy("this")
     private final AtomicInteger id = new AtomicInteger(0);
 
     public synchronized boolean add(User user) {
         storage.putIfAbsent(id.incrementAndGet(), new User(id.get(), user.getAmount()));
         return storage.get(id.get()).getAmount() == user.getAmount();
-    };
+    }
 
     public synchronized User get(int id) {
         return storage.get(id);
-    };
+    }
 
     public synchronized boolean update(User user) {
         if (storage.containsKey(user.getId())) {
@@ -28,7 +27,7 @@ public class UserStorage {
             return storage.get(user.getId()).getAmount() == user.getAmount();
         }
         return false;
-    };
+    }
 
     public synchronized boolean delete(User user) {
         if (storage.containsKey(user.getId())) {
@@ -36,7 +35,7 @@ public class UserStorage {
             return !storage.containsKey(user.getId());
         }
         return false;
-    };
+    }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         if (storage.get(fromId) != null && storage.get(toId) != null && storage.get(fromId).getAmount() > amount) {
