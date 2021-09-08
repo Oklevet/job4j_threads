a2502d23e84bc17717c08847458467e849a5f745
Oklevet <75887570+Oklevet@users.noreply.github.com>
2021-09-08 16:34:54 +0500

    0. CAS - операции [#6859 #200243]

--- src/main/java/buffer/CASCount.java
+++ src/main/java/buffer/CASCount.java
@@ -6,10 +6,10 @@ import java.util.concurrent.atomic.AtomicReference;
 @ThreadSafe
 public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
-    int current;
 
     public void increment() {
-        while(true) {
+        int current;
+        while (true) {
             current = count.get();
             if (count.compareAndSet(current, current + 1)) {
                 return;
--- src/test/java/buffer/CASCountTest.java
+++ src/test/java/buffer/CASCountTest.java
@@ -34,4 +34,18 @@ public class CASCountTest {
         th2.join();
         assertEquals(casCount.get(), 8);
     }
+
+    @Test
+    public void when3Thr() throws InterruptedException {
+        CASCount casCount = new CASCount();
+        ThreadAdd th1 = new ThreadAdd(casCount, 3);
+        ThreadAdd th2 = new ThreadAdd(casCount, 5);
+        ThreadAdd th3 = new ThreadAdd(casCount, 10);
+        th1.start();
+        th2.start();
+        th3.start();
+        th1.join();
+        th2.join();
+        th3.join();
+        assertEquals(casCount.get(), 18);    }
 }
