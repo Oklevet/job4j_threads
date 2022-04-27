e0272545f854d66bdab4079b1f05da96177587a5
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-04-27 12:24:33 +0500

    4. Скачивание файла с ограничением. [#144271 #176763]

---
+++ src/main/java/totalRecall/WgetArgValid.java
@@ -0,0 +1,65 @@
+package totalRecall;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WgetArgValid {
+    private String[] args;
+    public WgetArgValid(String[] args) {
+        this.args = args;
+        validation(args);
+    }
+
+    public void validation(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Arguments parameters: URL, minimum runtime, name output file.");
+        }
+        try {
+            URL u = new URL(args[0]);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("The selected URL is not valid.");
+        }
+        if (Integer.parseInt(args[1]) <= 0) {
+            throw new IllegalArgumentException("The specified program execution time must be positive.");
+        }
+        if (!isValidName(args[2])) {
+            throw new IllegalArgumentException("The file name is incorrect.");
+        }
+    }
+
+    public static boolean isValidName(String text)
+    {
+        Pattern pattern = Pattern.compile(
+                "# Match a valid Windows filename (unspecified file system).          \n" +
+                        "^                                # Anchor to start of string.        \n" +
+                        "(?!                              # Assert filename is not: CON, PRN, \n" +
+                        "  (?:                            # AUX, NUL, COM1, COM2, COM3, COM4, \n" +
+                        "    CON|PRN|AUX|NUL|             # COM5, COM6, COM7, COM8, COM9,     \n" +
+                        "    COM[1-9]|LPT[1-9]            # LPT1, LPT2, LPT3, LPT4, LPT5,     \n" +
+                        "  )                              # LPT6, LPT7, LPT8, and LPT9...     \n" +
+                        "  (?:\\.[^.]*)?                  # followed by optional extension    \n" +
+                        "  $                              # and end of string                 \n" +
+                        ")                                # End negative lookahead assertion. \n" +
+                        "[^<>:\"/\\\\|?*\\x00-\\x1F]*     # Zero or more valid filename chars.\n" +
+                        "[^<>:\"/\\\\|?*\\x00-\\x1F\\ .]  # Last char is not a space or dot.  \n" +
+                        "$                                # Anchor to end of string.            ",
+                Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.COMMENTS);
+        Matcher matcher = pattern.matcher(text);
+        boolean isMatch = matcher.matches();
+        return isMatch;
+    }
+
+    public String getUrl() {
+        return args[0];
+    }
+
+    public int getTime() {
+        return Integer.parseInt(args[1]);
+    }
+
+    public String getFileName() {
+        return args[2];
+    }
+}
---
+++ src/main/java/totalRecall/WgetV2.java
@@ -0,0 +1,51 @@
+package totalRecall;
+import java.io.*;
+import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
+
+public class WgetV2 implements Runnable{
+    private final String url;
+    private final String name;
+    private final long speed;
+    private final Instant start;
+
+    public WgetV2(String url, String name, int speed, Instant start) {
+        this.url = url;
+        this.name = name;
+        this.speed = speed * 1000L;
+        this.start = start;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream())) {
+            FileOutputStream fileOutputStream = new FileOutputStream(name);
+            byte[] dataBuff = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuff, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuff, 0, bytesRead);
+                long diffTime = Duration.between(start, Instant.now()).toMillis();
+                if (speed > diffTime) {
+                    Thread.sleep(diffTime);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml 1 download_temp.xml
+    public static void main(String[] args) throws InterruptedException {
+        WgetArgValid argValid = new WgetArgValid(args);
+        Instant start = Instant.now();
+        String url = argValid.getUrl();
+        int speed = argValid.getTime();
+        String name = argValid.getFileName();
+        Thread wget = new Thread(new WgetV2(url, name, speed, start));
+        wget.start();
+        wget.join();
+        //System.out.println("Duration: " + Duration.between(start, Instant.now()).toMillis());           // debug in console
+    }
+}
