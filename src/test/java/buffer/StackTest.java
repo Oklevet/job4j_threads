565073114aa75f179b41f854a826e5cd08af01d1
Oklevet <75887570+Oklevet@users.noreply.github.com>
2022-04-28 16:03:23 +0500

    Changes for Travis-CI

--- src/test/java/buffer/StackTest.java
+++ src/test/java/buffer/StackTest.java
@@ -3,7 +3,7 @@ package buffer;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class StackTest {
     @Test
