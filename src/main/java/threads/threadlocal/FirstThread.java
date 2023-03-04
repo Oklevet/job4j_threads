95f6e25ee09b40687239d31fc141be0523df0ece
Oklevet <nikstar1235@gmail.com>
2023-03-04 14:56:53 +0500

    9. ThreadLocal [#504912]

---
+++ src/main/java/threads/threadlocal/FirstThread.java
@@ -0,0 +1,9 @@
+package threads.threadlocal;
+
+public class FirstThread extends Thread {
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("Это поток 1.");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+}
---
+++ src/main/java/threads/threadlocal/SecondThread.java
@@ -0,0 +1,9 @@
+package threads.threadlocal;
+
+public class SecondThread extends Thread {
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("Это поток 2.");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+}
