0e2cebcc66bb24363f25a5d6bc9320ed402b0549
Oklevet <75887570+Oklevet@users.noreply.github.com>
2023-04-10 19:47:41 +0500

    10. Многопоточность в Stream API [#504930 #408552]

---
+++ src/main/java/concurrent/ParallelStreamExample.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ParallelStreamExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> stream = list.parallelStream();
+        System.out.println(stream.isParallel());
+        Optional<Integer> multiplication = stream.reduce((a, b) -> a * b);
+        System.out.println(multiplication.get());
+
+
+        IntStream parallel = IntStream.range(1, 100).parallel();
+        System.out.println(parallel.isParallel());
+        IntStream sequential = parallel.sequential();
+        System.out.println(sequential.isParallel());
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+        list1.stream().parallel().peek(System.out::println).toList();
+        System.out.println();
+        list1.stream().parallel().forEachOrdered(System.out::println);
+    }
+}
