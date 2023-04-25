8f775d592049a64873ac3654a2238a937cb1eda0
Oklevet <75887570+Oklevet@users.noreply.github.com>
2023-04-25 09:13:54 +0500

    1. Реализовать ThreadPool [#1099]

--- src/main/java/pool/ThreadPool.java
+++ src/main/java/pool/ThreadPool.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(2);
+    int size = Runtime.getRuntime().availableProcessors();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
     public ThreadPool() {
         Thread thread;
-        int size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
             thread = new Thread(() -> {
                 while (!Thread.currentThread().isInterrupted()) {
