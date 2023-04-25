ec56618b726b2bc5a2332d9d8ad2d70b34523137
Oklevet <75887570+Oklevet@users.noreply.github.com>
2023-04-25 09:36:31 +0500

    2. ExecutorService рассылка почты. [#63097]

--- src/main/java/pool/mailing/EmailNotification.java
+++ src/main/java/pool/mailing/EmailNotification.java
@@ -1,5 +1,19 @@
 package pool.mailing;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class EmailNotification {
-    public void close() { }
+    ExecutorService pool = Executors.newCachedThreadPool();
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void emailTo(User user) {
+        pool.submit(() -> send(String.format("Notification %s to email %s.", user.getUsername(), user.getEmail()),
+                String.format("Add new event to %s", user.getUsername()), user.getEmail()));
+    }
+
+    public void send(String subject, String body, String email) { }
 }
