59387f9514b0558bcdabf6c5187964b2c599d84a
Oklevet <nikstar1235@gmail.com>
2023-05-14 18:11:13 +0700

    4. CompletableFuture [#361626]

--- src/main/java/pool/RolColSum.java
+++ src/main/java/pool/RolColSum.java
@@ -9,15 +9,7 @@ public class RolColSum {
         return Arrays.stream(arr).sum();
     }
 
-    private static Sums[] getRows(int[][] matrix) {
-        Sums[] arr = new Sums[matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            arr[i] = new Sums(arrSumRow(matrix[i]), 0);
-        }
-        return arr;
-    }
-
-    private static Sums[] getCols(int[][] matrix) {
+    public static Sums[] getSumRowsCols(int[][] matrix) {
         int colSum = 0;
         Sums[] arr = new Sums[matrix.length];
 
@@ -25,35 +17,20 @@ public class RolColSum {
             for (int j = 0; j < matrix[i].length; j++) {
                 colSum += matrix[j][i];
             }
-            arr[i] = new Sums(1, colSum);
+            arr[i] = new Sums(arrSumRow(matrix[i]), colSum);
             colSum = 0;
-        }
-        return arr;
-    }
-
-    public static Sums[] sum(int[][] matrix) {
-        Sums[] arr;
-        Sums[] arrCols;
-        arr = getRows(matrix);
-        arrCols = getCols(matrix);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i].setColSum(arrCols[i].getColSum());
         }
         return arr;
     }
 
     public static CompletableFuture<Sums[]> getColsAsync(int[][] matrix) {
         return CompletableFuture.supplyAsync(
-                () -> getCols(matrix)
+                () -> getSumRowsCols(matrix)
         );
     }
 
     public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
         CompletableFuture<Sums[]> resArr = getColsAsync(matrix);
-        Sums[] sums = getRows(matrix);
-        for (int i = 0; i < sums.length; i++) {
-            sums[i].setColSum(resArr.get()[i].getColSum());
-        }
-        return sums;
+        return resArr.get();
     }
 }
--- src/test/java/pool/RolColSumTest.java
+++ src/test/java/pool/RolColSumTest.java
@@ -13,7 +13,7 @@ public class RolColSumTest {
     public void whenMatrix1Sync() {
         int[] arr1 = {1};
         int[][] matrix = {arr1};
-        Sums[] sums = sum(matrix);
+        Sums[] sums = getSumRowsCols(matrix);
         Sums[] expected = {new Sums(1, 1)};
         assertThat(sums, is(expected));
     }
@@ -25,7 +25,7 @@ public class RolColSumTest {
         int[] arr3 = {3, 3, 3, 3};
         int[] arr4 = {4, 4, 4, 4};
         int[][] matrix = {arr1, arr2, arr3, arr4};
-        Sums[] sums = sum(matrix);
+        Sums[] sums = getSumRowsCols(matrix);
         Sums[] expected = {new Sums(4, 10), new Sums(8, 10),
                 new Sums(12, 10), new Sums(16, 10)};
         assertThat(sums, is(expected));
@@ -41,30 +41,6 @@ public class RolColSumTest {
         Sums[] sums = asyncSum(matrix);
         Sums[] expected = {new Sums(4, 10), new Sums(8, 10),
                 new Sums(12, 10), new Sums(16, 10)};
-        assertThat(sums, is(expected));
-    }
-
-    @Test
-    public void whenMatrixUnsquareAsync() throws ExecutionException, InterruptedException {
-        int[] arr1 = {1, 1, 1};
-        int[] arr2 = {2, 2, 2};
-        int[] arr3 = {3, 3, 3};
-        int[] arr4 = {4, 4, 4};
-        int[][] matrix = {arr1, arr2, arr3, arr4};
-        Sums[] sums = asyncSum(matrix);
-        Sums[] expected = {new Sums(-1, -1)};
-        assertThat(sums, is(expected));
-    }
-
-    @Test
-    public void whenMatrixUnsquareSync() throws ExecutionException, InterruptedException {
-        int[] arr1 = {1, 1, 1};
-        int[] arr2 = {2, 2, 2};
-        int[] arr3 = {3, 3, 3};
-        int[] arr4 = {4, 4, 4};
-        int[][] matrix = {arr1, arr2, arr3, arr4};
-        Sums[] sums = sum(matrix);
-        Sums[] expected = {new Sums(-1, -1)};
         assertThat(sums, is(expected));
     }
 }
